51fa1ee92614ab92545f5c0e81943a297b5ab6c8
Isaac Santos <isaacdsantos@hotmail.com>
2022-08-03 12:22:59 -0300

    Incorporado app como dependencia em userRoute no arquivo index

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const userRoute = require('./routes/userRoute.js')
 const userRoutes = require('./routes/userRoute.js')
 const app = express()
 const porta = 3000
 
 app.get('/',(req,res)=>res.send('ola mundo'))
+
+userRoute(app)
 
 app.listen(porta,()=>console.log("Api funcionando"))
