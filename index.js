e565c84b14f6a37c831bca6b39cdc2eaa248df81
Isaac Santos <isaacdsantos@hotmail.com>
2022-08-03 15:26:51 -0300

    Adicionado funcao middleware em index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const bodyParse = require('body-parse')
 const userRoute = require('./routes/userRoute.js')
 const userRoutes = require('./routes/userRoute.js')
 const app = express()
+
 const porta = 3000
+
+app.use(bodyParser.urlencode({extend:false}))
 
 app.get('/',(req,res)=>res.send('ola mundo'))
 
